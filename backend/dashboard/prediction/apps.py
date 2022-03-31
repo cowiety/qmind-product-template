8cf9861d1d4e12276b232839da80ac3fd45dc908
cowiety <tyler.cowie1@gmail.com>
2022-03-31 18:58:54 -0400

    update backend

--- backend/dashboard/prediction/apps.py
+++ backend/dashboard/prediction/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+import tensorflow as tf
+import pandas as pd
+import os
 
 class PredictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'prediction'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    MLMODEL_PATH = os.path.join(BASE_DIR, 'prediction/mlmodel/BestModel')
+    mlmodel = tf.keras.models.load_model(MLMODEL_PATH)
